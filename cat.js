da7531626cabb7f0ddc4721cc9fb7916843a6221
kurmurthy <134779582+kurmurthy@users.noreply.github.com>
2024-03-06 16:55:29 +0530

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,15 @@
+function switchOff() {
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent = "switched Off";
+    document.getElementById("onSwitch").style.backgroundColor = "#22c55e";
+    document.getElementById("offSwitch").style.backgroundColor = "#7b8794";
+}
+
+function switchOn() {
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent = "switched On";
+    document.getElementById("offSwitch").style.backgroundColor = "#e12d39";
+    document.getElementById("onSwitch").style.backgroundColor = "#cbd2d9";
+}
